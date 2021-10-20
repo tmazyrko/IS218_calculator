4e6b281ae769a8a4228a46c5efef18e2bd0c9dae
tmazyrko <tmazyrko@gmail.com>
2021-10-20 14:03:46 -0400

    Add basic multiplication/division functions

--- calculator/main.py
+++ calculator/main.py
@@ -1,13 +1,15 @@
-""" This is the increment function"""
+""" Main calculator module """
 
 def inc(x_value):
     """ Increment Function adds one to the x_value"""
     return x_value + 1
 
+
 class Calculator:
     """ This is the Calculator class"""
 
     result = 0
+
     def get_result(self):
         """ Get Result of Calculation"""
         return self.result
@@ -16,7 +18,18 @@ class Calculator:
         """ adds number to result"""
         self.result = self.result + value_a
         return self.result
+
     def subtract_number(self, value_a):
         """ subtract number from result"""
         self.result = self.result - value_a
+        return self.result
+
+    def multiply_number(self, value_a):
+        """ multiply number by result """
+        self.result = self.result * value_a
+        return self.result
+
+    def divide_number(self, value_a):
+        """ divide result by number """
+        self.result = self.result / value_a
         return self.result
