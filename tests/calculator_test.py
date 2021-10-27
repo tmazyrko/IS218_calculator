598e8021086c77b2708d6fd8051ba90efa2b2bcb
tmazyrko <tmazyrko@gmail.com>
2021-10-27 14:03:56 -0400

    Add unit test for Calculator addition method

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,51 +2,6 @@
 from calc.main import Calculator
 
 
-def test_calculator_result():
-    """testing calc result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
-
-
-def test_calculator_get_result():
-    """Testing the Get result method of the calc"""
-    calc = Calculator()
-    calc.add_number(1)
-    assert calc.get_result() == 1
-
-
-def test_calculator_add():
-    """Testing the Add function of the calc"""
-    # Arrange by instantiating the calc class
-    calc = Calculator()
-    # Act by calling the method to be tested
-    calc.add_number(1)
-    # Assert that the results are correct
-    assert calc.result == 1
-
-
-def test_calculator_subtract():
-    """Testing the subtract method of the calc"""
-    calc = Calculator()
-    calc.subtract_number(1)
-    assert calc.get_result() == -1
-
-
-def test_calculator_multiply():
-    """Testing the multiply method of the calc"""
-    calc = Calculator()
-    calc.multiply_number(2)
-    assert calc.get_result() == 0
-
-
-def test_calculator_divide():
-    """Testing the divide method of the calc"""
-    calc = Calculator()
-    calc.divide_number(2)
-    assert calc.get_result() == 0
-
-def test_calculator_dividebyzero():
-    """Test for handling ZeroDivisionError exception"""
-    calc = Calculator()
-    calc.divide_number(0)
-    assert calc.get_result() == 0
+def test_calculator_add_static():
+    """Testing """
+    assert Calculator.add_numbers(1, 2) == 3
