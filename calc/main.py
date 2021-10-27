f05f774b731c0779c57e982900728a49235c3129
tmazyrko <tmazyrko@gmail.com>
2021-10-27 14:03:52 -0400

    Edit to use new Addition class

--- calc/main.py
+++ calc/main.py
@@ -1,23 +1,15 @@
 """ Main calc module """
-
-def inc(x_value):
-    """ Increment Function adds one to the x_value"""
-    return x_value + 1
+from calc.operations.addition import Addition
+from calc.operations.subtraction import Subtraction
 
 
 class Calculator:
     """ This is the Calculator class"""
 
-    result = 0
-
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.result
-
-    def add_number(self, value_a):
-        """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
+    @staticmethod
+    def add_numbers(value_a, value_b):
+        """ returns sum of two numbers """
+        return Addition.add(value_a, value_b)
 
     def subtract_number(self, value_a):
         """ subtract number from result"""
