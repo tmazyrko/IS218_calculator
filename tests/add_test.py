0a7a5f3f0034245f5d048813dd497aa4a273b3a2
tmazyrko <tmazyrko@gmail.com>
2021-10-27 14:10:42 -0400

    Delete old test

--- tests/add_test.py
+++
@@ -1,9 +0,0 @@
-""" content of calc.py#"""
-from calc.calculator import inc
-
-def test_answer():
-    """This Tests the function"""
-    assert inc(4) == 5
-def test_spam():
-    """This Tests the function"""
-    assert inc(4) == 5
