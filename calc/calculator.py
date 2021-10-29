d5a2eb7fe6c35ed21b2ec822f6b3b9b524b37944
tmazyrko <tmazyrko@gmail.com>
2021-10-29 13:11:32 -0400

    Add new Multiplication and Division classes

--- calc/calculator.py
+++ calc/calculator.py
@@ -1,6 +1,8 @@
 """ Main calc module """
 from calc.operations.addition import Addition
 from calc.operations.subtraction import Subtraction
+from calc.operations.multiplication import Multiplication
+from calc.operations.division import Division
 
 
 class Calculator:
@@ -15,3 +17,13 @@ class Calculator:
     def subtract_numbers(value_a, value_b):
         """ returns difference of two numbers """
         return Subtraction.subtract(value_a, value_b)
+
+    @staticmethod
+    def multiply_numbers(value_a, value_b):
+        """ returns multiple of two numbers """
+        return Multiplication.multiply(value_a, value_b)
+
+    @staticmethod
+    def divide_numbers(value_a, value_b):
+        """ returns division result of two numbers """
+        return Division.divide(value_a, value_b)
