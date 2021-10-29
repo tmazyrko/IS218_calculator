dfa9847ff007619ddd547e99b2c057aa9f70dfe5
tmazyrko <tmazyrko@gmail.com>
2021-10-29 13:13:59 -0400

    Add tests for new classes

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -9,3 +9,11 @@ def test_calculator_add_static():
 def test_calculator_subtract_static():
     """Testing """
     assert Calculator.subtract_numbers(5, 2) == 3
+
+def test_calculator_multiply_static():
+    """Testing """
+    assert Calculator.multiply_numbers(5, 2) == 10
+
+def test_calculator_divide_static():
+    """Testing """
+    assert Calculator.divide_numbers(6, 2) == 3
