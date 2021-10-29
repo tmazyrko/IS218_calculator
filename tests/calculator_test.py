4328178fd1fbbe73c2aa6c3ef3aba96e6627722a
tmazyrko <tmazyrko@gmail.com>
2021-10-29 13:55:32 -0400

    Add tests for Calculator history

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -17,3 +17,14 @@ def test_calculator_multiply_static():
 def test_calculator_divide_static():
     """Testing """
     assert Calculator.divide_numbers(6, 2) == 3
+
+def test_calculator_history_static_property():
+    """Testing Calculator history"""
+    prev_length = len(Calculator.history)
+    Calculator.add_numbers(1, 2)
+    assert len(Calculator.history) == prev_length + 1
+
+def test_calculator_history_get_addition_calculation():
+    """Testing ability to get Calculator object from history"""
+    calculation = Calculator.history[0]
+    assert calculation.get_result() == 3
