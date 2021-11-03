6e36337cdb82ed5a9e60173c564d75ea3a24caa0
tmazyrko <tmazyrko@gmail.com>
2021-11-03 10:31:35 -0400

    Update spacing for PEP8

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -6,23 +6,28 @@ def test_calculator_add_static():
     """Testing """
     assert Calculator.add_numbers(1, 2) == 3
 
+
 def test_calculator_subtract_static():
     """Testing """
     assert Calculator.subtract_numbers(5, 2) == 3
+
 
 def test_calculator_multiply_static():
     """Testing """
     assert Calculator.multiply_numbers(5, 2) == 10
 
+
 def test_calculator_divide_static():
     """Testing """
     assert Calculator.divide_numbers(6, 2) == 3
+
 
 def test_calculator_history_static_property():
     """Testing Calculator history"""
     prev_length = len(Calculator.history)
     Calculator.add_numbers(1, 2)
     assert len(Calculator.history) == prev_length + 1
+
 
 def test_calculator_history_get_addition_calculation():
     """Testing ability to get Calculator object from history"""
