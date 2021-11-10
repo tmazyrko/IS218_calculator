e853fdbd6975bb313f310a22c9298822aaf70236
tmazyrko <tmazyrko@gmail.com>
2021-11-09 20:29:52 -0500

    Refactor calculation base class

--- calc/operations/calculation.py
+++ calc/operations/calculation.py
@@ -1,11 +1,22 @@
-"""Calculation Class"""
+""" Calculation Class """
 
 
 class Calculation:  # pylint: disable=too-few-public-methods
-    """Calculation class"""
+    """ Calculation abstract base class """
 
     def __init__(self, *args):
-        """Default constructor"""
-        self.values = []
-        for arg in args:
-            self.values.append(arg)
+        """ Default constructor """
+        self.values = Calculation.convert_args_to_tuple_of_float(values)
+
+    @classmethod
+    def create(cls, values: tuple):
+        """ Factory method """
+        return cls(values)
+
+    @staticmethod
+    def convert_args_to_tuple_of_float(values):
+        """ Standardizes values to a list of floats """
+        list_values_float = []
+        for item in values:
+            list_values_float.append(float(item))
+        return tuple(list_values_float)
