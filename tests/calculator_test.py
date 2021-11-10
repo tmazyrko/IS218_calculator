7cacadcad0e7565c0e7f1f68a4dcdfe3ea6350fb
tmazyrko <tmazyrko@gmail.com>
2021-11-09 20:41:50 -0500

    Rework Calculator tests

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,77 +1,38 @@
-"""Testing the Calculator"""
+""" Testing the Calculator """
+import pytest
 from calc.calculator import Calculator
+from calc.history.calculations import Calculations
 
 
-def test_calculator_add_static():
-    """Testing """
-    assert Calculator.add_numbers(1, 2) == 3
+@pytest.fixture
+def clear_history_fixture(): # pylint: disable=redefined-outer-name
+    """ Fixture for clearing Calculations history """
+    Calculations.clear_history()
 
 
-def test_calculator_subtract_static():
-    """Testing """
-    assert Calculator.subtract_numbers(5, 2) == 3
+def test_calculator_add_static(clear_history_fixture): # pylint: disable=unused-argument,redefined-outer-name
+    """ Testing that the calculator has a static method for addition """
+    values = (1.0, 2.0, 5.0)
+    Calculator.add_numbers(values)
+    assert Calculator.get_last_result_value() == 8.0
 
 
-def test_calculator_multiply_static():
-    """Testing """
-    assert Calculator.multiply_numbers(5, 2) == 10
+def test_calculator_subtract_static(clear_history_fixture): # pylint: disable=unused-argument,redefined-outer-name
+    """ Testing that the calculator has a static method for subtraction """
+    values = (1.0, 2.0, 5.0)
+    Calculator.subtract_numbers(values)
+    assert Calculator.get_last_result_value() == -6.0
 
 
-def test_calculator_divide_static():
-    """Testing """
-    assert Calculator.divide_numbers(6, 2) == 3
+def test_calculator_multiply_static(clear_history_fixture): # pylint: disable=unused-argument,redefined-outer-name
+    """ Testing that the calculator has a static method for multiplication """
+    values = (3.0, 2.0, 5.0)
+    Calculator.multiply_numbers(values)
+    assert Calculator.get_last_result_value() == 30.0
 
 
-def test_calculator_history_static_property():
-    """Testing Calculator history"""
-    prev_length = len(Calculator.history)
-    Calculator.add_numbers(1, 2)
-    assert len(Calculator.history) == prev_length + 1
-
-
-def test_calculator_history_get_addition_calculation():
-    """Testing ability to get Calculator object from history"""
-    calculation = Calculator.history[0]
-    assert calculation.get_result() == 3
-
-
-def test_calculator_history_get_length():
-    """Testing method returning length of history list"""
-    length = Calculator.history_length()
-    assert length == 5
-
-
-def test_calculator_history_get_first_obj():
-    """Testing method getting first object from history"""
-    assert Calculator.get_first_history_obj() == Calculator.history[0]
-
-
-def test_calculator_history_get_first_result():
-    """Testing method getting result of first calculation in history"""
-    assert Calculator.get_first_history_result() == 3
-
-
-def test_calculator_history_get_last_obj():
-    """Testing method getting last object from history"""
-    assert Calculator.get_last_history_obj() == Calculator.history[Calculator.history_length() - 1]
-
-
-def test_calculator_history_get_last_result():
-    """Testing method getting result of last calculation in history"""
-    result = Calculator.add_numbers(7, 4)
-    assert Calculator.get_last_history_result() == result
-
-
-def test_calculator_history_remove_calculation():
-    """Testing method to remove single calculation from history"""
-    pre_length = Calculator.history_length()
-    Calculator.history_remove(0)
-    post_length = Calculator.history_length()
-    assert post_length == pre_length - 1
-
-
-def test_calculator_clear_history():
-    """Testing clear history method"""
-    Calculator.clear_history()
-    length = Calculator.history_length()
-    assert length == 0
+def test_calculator_divide_static(clear_history_fixture): # pylint: disable=unused-argument,redefined-outer-name
+    """ Testing that the calculator has a static method for division """
+    values = (20.0, 2.0, 2.0)
+    Calculator.add_numbers(values)
+    assert Calculator.get_last_result_value() == 5.0
