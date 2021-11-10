f8b163e4d2d52cb5364f0d2757125917f1b1061f
tmazyrko <tmazyrko@gmail.com>
2021-11-09 20:43:31 -0500

    Fix PEP8 comment spacing

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -5,33 +5,33 @@ from calc.history.calculations import Calculations
 
 
 @pytest.fixture
-def clear_history_fixture(): # pylint: disable=redefined-outer-name
+def clear_history_fixture():  # pylint: disable=redefined-outer-name
     """ Fixture for clearing Calculations history """
     Calculations.clear_history()
 
 
-def test_calculator_add_static(clear_history_fixture): # pylint: disable=unused-argument,redefined-outer-name
+def test_calculator_add_static(clear_history_fixture):  # pylint: disable=unused-argument,redefined-outer-name
     """ Testing that the calculator has a static method for addition """
     values = (1.0, 2.0, 5.0)
     Calculator.add_numbers(values)
     assert Calculator.get_last_result_value() == 8.0
 
 
-def test_calculator_subtract_static(clear_history_fixture): # pylint: disable=unused-argument,redefined-outer-name
+def test_calculator_subtract_static(clear_history_fixture):  # pylint: disable=unused-argument,redefined-outer-name
     """ Testing that the calculator has a static method for subtraction """
     values = (1.0, 2.0, 5.0)
     Calculator.subtract_numbers(values)
     assert Calculator.get_last_result_value() == -6.0
 
 
-def test_calculator_multiply_static(clear_history_fixture): # pylint: disable=unused-argument,redefined-outer-name
+def test_calculator_multiply_static(clear_history_fixture):  # pylint: disable=unused-argument,redefined-outer-name
     """ Testing that the calculator has a static method for multiplication """
     values = (3.0, 2.0, 5.0)
     Calculator.multiply_numbers(values)
     assert Calculator.get_last_result_value() == 30.0
 
 
-def test_calculator_divide_static(clear_history_fixture): # pylint: disable=unused-argument,redefined-outer-name
+def test_calculator_divide_static(clear_history_fixture):  # pylint: disable=unused-argument,redefined-outer-name
     """ Testing that the calculator has a static method for division """
     values = (20.0, 2.0, 2.0)
     Calculator.add_numbers(values)
