16abdd18adbce1f5e048ca68b16379dd7fb9fdfe
tmazyrko <tmazyrko@gmail.com>
2021-11-12 14:00:44 -0500

    Update to use create method for instantiating Operations

--- tests/subtraction_test.py
+++ tests/subtraction_test.py
@@ -5,5 +5,5 @@ from calc.operations.subtraction import Subtraction
 def test_operation_subtraction():
     """ Tests subtraction """
     values = (5.0, 2.0)
-    subtraction = Subtraction(values)
+    subtraction = Subtraction.create(values)
     assert subtraction.get_result() == 3.0
