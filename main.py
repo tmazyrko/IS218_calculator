ec8868684ed79b324bf9500f426d1e844593952a
tmazyrko <tmazyrko@gmail.com>
2021-12-01 20:22:28 -0500

    Create main program with input directory observer

---
+++ main.py
@@ -0,0 +1,53 @@
+from calc.history.calculations import Calculations
+from calc.operations.addition import Addition
+from calc.operations.subtraction import Subtraction
+from calc.operations.multiplication import Multiplication
+from calc.operations.division import Division
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Watcher:
+    DIRECTORY_TO_WATCH = "data/input"
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        print("Started observer.")
+        try:
+            while True:
+                time.sleep(5)
+        except KeyboardInterrupt:
+            self.observer.stop()
+            self.observer.join()
+            print("Exiting.")
+        except:
+            self.observer.stop()
+            self.observer.join()
+            print("Error")
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print("Received created event - %s." % event.src_path)
+
+        # elif event.event_type == 'modified':
+            # Take any action here when a file is modified.
+            # print("Received modified event - %s." % event.src_path)
+
+
+if __name__ == '__main__':
+    w = Watcher()
+    w.run()
