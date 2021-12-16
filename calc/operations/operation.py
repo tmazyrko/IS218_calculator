259ebed05363b9120a754217063b5a3b0c4993e3
tmazyrko <tmazyrko@gmail.com>
2021-12-15 19:09:37 -0500

    Add timestamps to operations

--- calc/operations/operation.py
+++ calc/operations/operation.py
@@ -1,4 +1,6 @@
 """ Operation Class """
+import datetime
+import pytz
 
 
 class Operation:  # pylint: disable=too-few-public-methods
@@ -6,7 +8,9 @@ class Operation:  # pylint: disable=too-few-public-methods
 
     def __init__(self, values: tuple):
         """ Default constructor """
+        self.tz = 'America/New_York'  # US East timezone
         self.values = Operation.convert_args_to_tuple_of_float(values)
+        self.timestamp = self.set_timestamp(self.tz)
 
     @classmethod
     def create(cls, values: tuple):
@@ -20,3 +24,14 @@ class Operation:  # pylint: disable=too-few-public-methods
         for item in values:
             list_values_float.append(float(item))
         return tuple(list_values_float)
+
+    @staticmethod
+    def set_timestamp(tz):
+        """ Returns a formatted timestamp for the creation of an operation """
+        timestamp = datetime.datetime.now(pytz.timezone(tz))
+        formatted_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S (UTC%z)")
+        return formatted_timestamp
+
+    def get_timestamp(self):
+        """ Retrieves stored timestamp """
+        return self.timestamp
